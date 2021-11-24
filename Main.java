80db17fa6a8a3c67bf1bac89ade648625aa2f0a0
Ritik kumar <54769957+ritikbaijnath920@users.noreply.github.com>
2021-11-24 16:32:33 +0000

    Space

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
-class Main {
+import java.util.*;
+
+public class Main {
+
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    // Write your code here
+    System.out.println("*****");
+    System.out.println("   *");
+    System.out.println("  *");
+    System.out.println(" *");
+    System.out.println("*****");
+
   }
 }
